fa9625e4de2b7d651b328271a73bf1652debe346
Kpsinghhere <kannu089btcse18@igdtuw.ac.in>
2024-08-10 21:01:00 +0530

    commit

---
+++ script.js
@@ -0,0 +1,260 @@
+//made by vipul mirajkar thevipulm.appspot.com
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+//counter
+
+(function($) {
+		"use strict";
+		function count($this){
+		var current = parseInt($this.html(), 10);
+		current = current + 1; /* Where 50 is increment */	
+		$this.html(++current);
+			if(current > $this.data('count')){
+				$this.html($this.data('count'));
+			} else {    
+				setTimeout(function(){count($this)}, 50);
+			}
+		}        	
+		$(".stat-count").each(function() {
+		  $(this).data('count', parseInt($(this).html(), 10));
+		  $(this).html('0');
+		  count($(this));
+		});
+   })(jQuery);
+
+
+//progress bar js
+
+$(document).ready(function ($) {
+    function animateElements() {
+        $('.progressbar').each(function () {
+            var elementPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            var percent = $(this).find('.circle').attr('data-percent');
+            var animate = $(this).data('animate');
+            if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+                $(this).data('animate', true);
+                $(this).find('.circle').circleProgress({
+                    // startAngle: -Math.PI / 2,
+                    value: percent / 100,
+                    size : 400,
+                    thickness: 15,
+                    fill: {
+                        color: '#f47a14'
+                    }
+                }).on('circle-animation-progress', function (event, progress, stepValue) {
+                    $(this).find('strong').text((stepValue*100).toFixed(0) + "%");
+                }).stop();
+            }
+        });
+    }
+
+    animateElements();
+    $(window).scroll(animateElements);
+});
+
+//accordion
+
+$("#faq-accordion").accordion({
+collapsible : true,
+active : 0,
+heightStyle: "true",
+icons : {header:"ui-icon-plus", activeheader:"ui-icon-minus"}
+});
+
+//owl carouserl slider
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    
+    dots: true,
+    autoplay: true,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+/*smoth scroling*/
+
+$(document).ready(function(){
+  $('.menu a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+
+// fixed header 
+
+jQuery(window).scroll(function(){
+    if (jQuery(window).scrollTop() >= 10) {
+       jQuery('#home').addClass('fixed-header');
+    }
+    else {
+       jQuery('#home').removeClass('fixed-header');
+    }
+});
+
+/*scroll to top*/
+$(document).ready(function(){
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100) {
+        $("#arrow i").fadeIn();
+}
+    else {
+        $("#arrow i").fadeOut();
+}
+
+});
+$("#arrow i").on('click',function(){
+$("html,body").animate({
+    scrollTop: 0
+}, 600);
+    return false;
+});
+
+
+let colors = ['#3b3663', 'red', 'blue', '#FFFF00']; 
+let index = 0;
+
+function changeColor() {
+    let textElement = document.getElementById('changingText');
+    textElement.style.color = colors[index];
+    index = (index + 1) % colors.length; // Increment index and loop back to 0 if it exceeds the array length
+}
+
+setInterval(changeColor, 1000); // Change color every 5 seconds
+
+
+let images = ['url(../images/bg-image1.jpeg)', 'url(../images/bg-image2.avif)', 'url(../images/bg-image3.jpeg)'];
+let bgIndex = 0;
+let headerCol2 = document.getElementById('headerCol2');
+
+function changeBackgroundImage() {
+    headerCol2.style.backgroundImage = images[bgIndex];
+    bgIndex = (bgIndex + 1) % images.length; // Increment index and loop back to 0 if it exceeds the array length
+}
+
+setInterval(changeBackgroundImage, 20000); // Change background image every 5 seconds
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var swiper = new swiper('.gallery', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+});
